eb5b11850231709cfc6e8f6d68fbe3703236b3d2
Aymene Bousbia <aymene01@hotmail.fr>
2023-02-22 20:27:53 +0100

    setup web app

---
+++ apps/web/src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <h1 className="text-7xl font-light">Hi Blindtest</h1>
+    </div>
+  )
+}
